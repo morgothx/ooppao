c4235df9f2ce46cfa7ffe972cf06c5bd4302f13c
paola <paolisco2525@gmail.com>
2024-04-21 12:59:25 -0500

    Practice today

--- exceptions.php
+++ exceptions.php
@@ -1,35 +1,45 @@
 <?php
 
-function divide($dividend, $divisor) {
-    if ($divisor == 0) {
+function divide($dividend, $divisor)
+{
+    if ($divisor == 0)
+    {
         throw new Exception("División by zero");
     }
 
-    if (!is_numeric($dividend) || !is_numeric($divisor)) {
+    if (!is_numeric($dividend) || !is_numeric($divisor))
+    {
         throw new \http\Exception\InvalidArgumentException("Invalid input. Both dividend and divisor must be numeric");
     }
 
-    if ($dividend % $divisor != 0) {
+    if ($dividend % $divisor != 0)
+    {
         throw new \http\Exception\RuntimeException("The result of the divison is not an interger.");
     }
 
     return $dividend / $divisor;
 }
 
-try {
+try
+{
     echo divide(10, 0);
-} catch (Exception $e) {
+} catch (Exception $e)
+{
     echo "Caught exception: " .$e->getMessage() . "\n";
 }
 
-try {
+try
+{
     echo divide(20, 'a');
-} catch (InvalidArgumentException $e) {
+} catch (InvalidArgumentException $e)
+{
     echo "Caught InvalidArgumentException :" . $e->getMessage() . "\n";
 }
 
-try {
+try
+{
     echo divide(15, 7);
-} catch (RuntimeException $e) {
+} catch (RuntimeException $e)
+{
     echo "Caught RuntimeException: " . $e->getMessage() . "\n";
 }
